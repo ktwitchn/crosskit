490f22736f882e79cfeaeb461ecffc3b9d3dbf23
Kenny Twitchell <kennytwitchell@gmail.com>
2024-08-11 17:09:40 -0600

    stores

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { count } from '$lib/stores/counts';
+
+	let count_value: number;
+	count.subscribe((value) => (count_value = value));
+
+	function handleClick() {
+		count.update((n) => n + 1);
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count_value}
+	{count_value === 1 ? 'time' : 'times'}
+</button>
---
+++ src/routes/count/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+	import Value from '$lib/components/Value.svelte';
+</script>
+
+<Card></Card>
+<Value></Value>
