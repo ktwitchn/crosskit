e26feeb226d1135c631665538c5e52f3762bc911
Kenny Twitchell <kennytwitchell@gmail.com>
2024-08-11 19:42:53 -0600

    adding scroll area to puzzle list

--- src/lib/components/mol/puzzles/puzzles.svelte
+++ src/lib/components/mol/puzzles/puzzles.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
 	import { createPuzzle } from '$lib/helpers/puzzlehelper';
 	import { puzzles, type Puzzle } from '$lib/stores/puzzles';
 
@@ -22,14 +23,18 @@
 		<Button on:click={handleCreatePuzzle}>New</Button>
 	</div>
 
-	{#each puzzles_val as puzzle}
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>{puzzle.name}</Card.Title>
-				<Card.Description>{puzzle.description}</Card.Description>
-			</Card.Header>
-		</Card.Root>
-	{/each}
+	<ScrollArea>
+		<div class="puzzles-list">
+			{#each puzzles_val as puzzle}
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>{puzzle.name}</Card.Title>
+						<Card.Description>{puzzle.description}</Card.Description>
+					</Card.Header>
+				</Card.Root>
+			{/each}
+		</div>
+	</ScrollArea>
 </div>
 
 <style>
@@ -39,6 +44,13 @@
 		gap: 1em;
 		width: 100%;
 		min-width: 15em;
+	}
+	.puzzles-list {
+		height: 90vh;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+		margin-right: 1em;
 	}
 	@media screen and (min-width: 550px) {
 		.puzzles-container {
